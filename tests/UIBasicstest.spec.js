c2fa26c226588caec6fbe645136dc98be3f0fd4f
Martin Gonzales <aabravoc@gmail.com>
2025-10-25 16:13:49 -0500

    subiendo proyecto inicial

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-test.only('Browser context playwright test',async ({browser})=>
+test('Browser context playwright test',async ({browser})=>
 {
     
     const context = await browser.newContext();
@@ -8,6 +8,7 @@ test.only('Browser context playwright test',async ({browser})=>
 
     const userName = page.locator("#username");
     const btnSignIn = page.locator("#signInBtn");
+    const cardTitles = page.locator(".card-body a");
 
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
@@ -23,8 +24,14 @@ test.only('Browser context playwright test',async ({browser})=>
     await userName.fill("");
     await userName.fill("rahulshettyacademy");
     await btnSignIn.click();
-    console.log(await page.locator(".card-body a").first().textContent());
-    console.log(await page.locator(".card-body a").nth(1).textContent());
+   console.log(await cardTitles.first().textContent());
+    console.log(await cardTitles.nth(1).textContent());
+    
+
+    const allTitles = await cardTitles.allTextContents();
+    console.log("allTitles: "+allTitles);
+
+
 });
 
 
@@ -34,3 +41,59 @@ test('Page playwright test',async ({page})=>
     console.log(await page.title());
     await expect(page).toHaveTitle("Google");
 });
+
+test('UI Controls', async({page}) =>
+{
+    const userName = page.locator("#username");
+    const btnSignIn = page.locator("#signInBtn");
+    const dropdown = page.locator("select.form-control");
+    const documentLink = page.locator("[href*='documents-request']");
+   
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    await page.locator("[type='password']").fill("learning");
+    await userName.fill("rahulshettyacademy");
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    console.log("aquí: "+await page.locator(".radiotextsty").last().isChecked());
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+
+    await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+
+    await page.locator("#terms").uncheck();
+    expect( await page.locator("#terms").isChecked()).toBeFalsy();
+
+    await expect(documentLink).toHaveAttribute("class","blinkingText");  
+   // await btnSignIn.click();
+   // await page.pause();
+}
+);
+
+
+test.only('Child Windows', async ({browser}) => 
+    {
+
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        
+
+        await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+        const documentLink = page.locator("[href*='documents-request']");
+
+        
+        const [newPage] = await Promise.all([
+            context.waitForEvent('page'),
+            documentLink.click(),
+        ]) 
+
+        const text = await newPage.locator(".red").textContent();
+        const arrayText = text.split("@");
+        const domain = arrayText[1].split(" ")[0];
+        //console.log(domain);
+
+        const userName = page.locator("#username");
+        await userName.fill(domain);
+        console.log(await page.locator("#username").inputValue());
+    });
